03cb0e6476d72b8d193c12a197e562bf4073c76b
batl <max.toloknov@gmail.com>
2012-03-23 15:12:46 +0200

    db dump

---
+++ wp-content/themes/twentyeleven/template_page-contacts.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Template Name: Contacts
+*/
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>					
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/twentyeleven/template_page-projects.php
@@ -0,0 +1,34 @@
+<?php
+/*
+	Template Name: Projects
+*/
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<?php endwhile; ?>
+
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+				
+					<p><?php _e( 'Ни одного проекта не найдено.', 'twentyeleven' ); ?></p>					
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
